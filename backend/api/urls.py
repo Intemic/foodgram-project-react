eb5b80ea1e328e5811f902757c76a52201219490
Антон Лучик <Intemic@yandex.ru>
2023-09-08 19:26:02 +0500

    Устранение замечаний

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import include, path
-from djoser.views import UserViewSet as DjoserViewSet
 from rest_framework.routers import DefaultRouter
 
 from .views import IngredientViewSet, RecipeViewSet, TagViewSet
@@ -13,10 +12,6 @@ router_v1.register('tags', TagViewSet, basename='tags')
 router_v1.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
-    path(
-        'users/set_password/',
-        DjoserViewSet.as_view({'post': 'set_password'})
-    ),
     path('', include(router_v1.urls)),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
